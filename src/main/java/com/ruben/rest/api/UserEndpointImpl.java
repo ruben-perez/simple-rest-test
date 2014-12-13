ecee15648470c658498877c32596cc44d10381ab
ruben-perez <ruben.perezg@gmail.com>
2014-12-13 04:00:55 +0100

    Added needed rest annotations

--- src/main/java/com/ruben/rest/api/UserEndpointImpl.java
+++ src/main/java/com/ruben/rest/api/UserEndpointImpl.java
@@ -1,6 +1,8 @@
 package com.ruben.rest.api;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ruben.rest.api.response.UserResponse;
@@ -10,6 +12,8 @@ import com.ruben.rest.api.response.UserResponse;
 public class UserEndpointImpl implements UserEndpoint {
 
 	@Override
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    @ResponseBody
 	public UserResponse getUsers() {
 		return new UserResponse(1, "name", "email");
 	}
